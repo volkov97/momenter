bf03156877eaaa907de4cf561ebcf08398d54846
volkov97 <volkov97@yandex-team.ru>
2019-08-27 17:40:36 +0300

    styles with 2 screens

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ const momentInput = document.querySelector('.moment-input') as HTMLInputElement;
 const startButton = document.querySelector('.controls__button_start') as HTMLButtonElement;
 const countdownElement = document.querySelector('.countdown-value') as HTMLDivElement;
 
-momentInput.value = '27.08.2019 16:27';
+momentInput.value = '27.08.2019 21:00';
 
 startButton.addEventListener('click', () => {
   const { value } = momentInput;
@@ -17,15 +17,26 @@ startButton.addEventListener('click', () => {
 
   const date = new Date(year, month - 1, day, hours, minutes);
 
-  const interval = setInterval(() => {
-    const currentTime = Date.now();
+  (document.querySelector('.screen.visible') as HTMLDivElement).classList.remove('visible');
+  (document.querySelector('.screen.screen_countdown') as HTMLDivElement).classList.add('visible');
 
-    if (currentTime > date.getTime()) {
+  function renderTime() {
+    const currentTime = Date.now();
+    const { h, m, s } = getTimeParts(date.getTime() - currentTime, 'h');
+    const prefixTimePart = (t: number) => (t > 9 ? t : `0${s}`);
+
+    countdownElement.innerText = `${h}:${prefixTimePart(m)}:${prefixTimePart(s)}`;
+  }
+
+  renderTime();
+
+  const interval = setInterval(() => {
+    if (Date.now() > date.getTime()) {
       clearInterval(interval);
 
       return;
     }
 
-    countdownElement.innerText = JSON.stringify(getTimeParts(date.getTime() - currentTime, 'd'));
+    renderTime();
   }, 1000);
 });
