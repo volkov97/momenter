a4f0669fec24d7775d4c4c4b45e3d069c68955fe
volkov97 <volkov97@yandex-team.ru>
2019-08-27 16:39:57 +0300

    plain example of countdown

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,31 @@
 import './index.less';
 
-console.log('heelooo world111!!');
+import getTimeParts from './lib/getTimeParts';
+
+const momentInput = document.querySelector('.moment-input') as HTMLInputElement;
+const startButton = document.querySelector('.controls__button_start') as HTMLButtonElement;
+const countdownElement = document.querySelector('.countdown-value') as HTMLDivElement;
+
+momentInput.value = '27.08.2019 16:27';
+
+startButton.addEventListener('click', () => {
+  const { value } = momentInput;
+
+  const [dayString, timeString] = value.split(' ');
+  const [day, month, year] = dayString.split('.').map(num => parseInt(num));
+  const [hours, minutes] = timeString.split(':').map(num => parseInt(num));
+
+  const date = new Date(year, month - 1, day, hours, minutes);
+
+  const interval = setInterval(() => {
+    const currentTime = Date.now();
+
+    if (currentTime > date.getTime()) {
+      clearInterval(interval);
+
+      return;
+    }
+
+    countdownElement.innerText = JSON.stringify(getTimeParts(date.getTime() - currentTime, 'd'));
+  }, 1000);
+});
---
+++ src/types/index.ts
@@ -0,0 +1,9 @@
+export type Unit = 'ms' | 's' | 'm' | 'h' | 'd';
+
+export interface TimeParts {
+  ms: number;
+  s: number;
+  m: number;
+  h: number;
+  d: number;
+}
